46470b57a9b8fab294f43cbeca44061afe9088d7
Eugene Peralta <157940784+Shishwami@users.noreply.github.com>
2025-05-13 20:49:45 +0800

    Update submitSnippet.js

--- api/submitSnippet.js
+++ api/submitSnippet.js
@@ -1,8 +1,20 @@
-import clientPromise from "../lib/mongodb";
+import clientPromise from "../lib/mongodb.js";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
     try {
+        const client = await clientPromise;
+        const db = client.db(process.env.DB_NAME);
+        const collection = db.collection(process.env.COLLECTION);
 
+        const { code, language } = req.query;
+        const codeSnippet = {
+            code: code,
+            language: language,
+            dateCreated: new Date
+        }
+
+        const result = await collection.insertOne(codeSnippet);
+        res.status(201).json({ id: result.insertedId });
     } catch (err) {
         console.error("API error:", err);
         res.status(500).json({ error: "Internal Server Error", details: err.message });

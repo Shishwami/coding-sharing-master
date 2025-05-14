a8118f15c45140dedf57adffaaf281cab43f058a
Eugene Peralta <157940784+Shishwami@users.noreply.github.com>
2025-05-14 12:01:55 +0800

    Update defaults.js

--- src/scripts/defaults.js
+++ src/scripts/defaults.js
@@ -5,9 +5,22 @@ export const language = localStorage.getItem('language') || 'html';
 export const themes = [
     {
         id: 'custom-light',
+        base: 'vs',
+        aliases:['Light'],
+        inherit: true,
+        rules:[],
         backgroundColor: "#FFFFFE"
+    },
+    {
+        id: 'custom-dark',
+        aliases:['VS-Dark'],
+        base: 'vs-dark',
+        inherit: true,
+        rules:[],
+        backgroundColor: "#121826"
     }
 ];
+
 
 export const theme = localStorage.getItem('theme') || themes[0].id;
 

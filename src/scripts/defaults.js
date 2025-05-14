5d26ddc27af977a699a309e17f34a40e064ae8c5
Eugene Peralta <157940784+Shishwami@users.noreply.github.com>
2025-05-15 02:45:01 +0800

    Update defaults.js

--- src/scripts/defaults.js
+++ src/scripts/defaults.js
@@ -1,4 +1,21 @@
-export const code = localStorage.getItem('code') || "//type something here;"
+export const code = localStorage.getItem('code') || `<html>
+  <head>
+    <title>HTML Sample</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <style type="text/css">
+      h1 {
+        color: #CCA3A3;
+      }
+    </style>
+    <script type="text/javascript">
+      alert("I am a sample... visit devChallengs.io for more projects");
+    </script>
+  </head>
+  <body>
+    <h1>Heading No.1</h1>
+    <input disabled type="button" value="Click me" />
+  </body>
+</html>`;
 export const language = localStorage.getItem('language') || 'html';
 
 
@@ -6,17 +23,17 @@ export const themes = [
     {
         id: 'custom-light',
         base: 'vs',
-        aliases:['Light'],
+        aliases: ['Light'],
         inherit: true,
-        rules:[],
+        rules: [],
         backgroundColor: "#FFFFFE"
     },
     {
         id: 'custom-dark',
-        aliases:['VS-Dark'],
+        aliases: ['VS-Dark'],
         base: 'vs-dark',
         inherit: true,
-        rules:[],
+        rules: [],
         backgroundColor: "#121826"
     }
 ];
